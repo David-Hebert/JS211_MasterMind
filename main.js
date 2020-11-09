ca5f9c82113f92dcc9e9160f6e6a7a65c6dc1f4b
David-Hebert <king.david.abear@zoho.com>
2020-11-08 23:20:30 -0600

    testin

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 // 'use strict';
 
-// const assert = require('assert');
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
 
